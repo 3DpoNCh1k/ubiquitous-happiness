eaa5d190e78a841642628b65f1d84fffadaaa36d
Maxim Polyashov <polyashovmaxim@gmail.com>
2020-12-09 04:08:25 +0300

    stepik_sql5.6 and w/o pathlib in settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # print("in settings")
 # print(BASE_DIR)
--- stepik_preproc_2.sh
+++ stepik_preproc_2.sh
@@ -1,2 +1,10 @@
-conda install django
+python -m pip install django
 python -m pip install mysqlclient
+
+sudo echo "#!/bin/sh
+exit 0" > /usr/sbin/policy-rc.d
+
+sudo apt-get update
+sudo apt-get install mysql-server-5.6
+
+
